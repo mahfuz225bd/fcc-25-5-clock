daa0f9f98b6b04ac046692ce1e681668f3da963d
mahfuz225bd <mahfuz225bd@gmail.com>
2022-12-31 21:26:58 +0600

    component: InputSpinner added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,40 @@ import InputSpinner from './components/InputSpinner'
 import Display from './components/Display'
 
 class App extends Component {
+  state = {
+    breakValue: 5,
+    sessionValue: 25,
+    leftTimeFor: 'Session',
+    leftTime: 0,
+    isPlaying: true
+  }
+
+  incrementBreakValue() {
+    let value = this.state.breakValue
+    value++;
+    this.setState({ breakValue: value })
+  }
+
+  decrementBreakValue() {
+    let value = this.state.breakValue
+    if (value > 0) {
+      value--;
+      this.setState({ breakValue: value })
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ leftTime: this.state.sessionValue })
+  }
 
   render() {
+    const { breakValue, sessionValue, leftTimeFor, leftTime } = this.state
     return (
       <div className='container'>
         <h1>25 + 5 Clock</h1>
         <main>
           <div className='input-times'>
-            <InputSpinner />
-            <InputSpinner />
+            <InputSpinner label='Break Length' value={breakValue} increment={this.incrementBreakValue.bind(this)} decrement={this.decrementBreakValue.bind(this)} />
           </div>
           <Display />
           <div className='action-buttons'>
--- src/components/InputSpinner/index.jsx
+++ src/components/InputSpinner/index.jsx
@@ -1,10 +1,30 @@
+import PropTypes from "prop-types"
 import React from 'react'
 import styles from './InputSpinner.module.css'
 
-const InputSpinner = () => {
+const InputSpinner = ({ label, value, increment, decrement }) => {
     return (
-        <div className={styles.container}>InputSpinner</div>
+        <div className={styles.container}>
+            <h3>{label}</h3>
+            <section>
+                <a onClick={increment}>🔼</a>
+                <span className={styles.value}>{value}</span>
+                <a onClick={decrement}>🔽</a>
+            </section>
+        </div>
     )
+}
+
+InputSpinner.propTypes = {
+    decrement: PropTypes.func.isRequired,
+    increment: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired
+}
+
+InputSpinner.defaultProps = {
+    value: 0,
+    label: 'Untitled'
 }
 
 export default InputSpinner
