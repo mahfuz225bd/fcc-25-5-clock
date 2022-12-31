05fc918c126ccc1c36108792f406aae05436f551
mahfuz225bd <mahfuz225bd@gmail.com>
2022-12-31 14:29:53 +0600

    basic layout designed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      25 + 5 Clock App
-    </div>
-  );
+import InputSpinner from './components/InputSpinner'
+import Display from './components/Display'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='container'>
+        <h1>25 + 5 Clock</h1>
+        <main>
+          <div className='input-times'>
+            <InputSpinner />
+            <InputSpinner />
+          </div>
+          <Display />
+          <div className='action-buttons'>
+            <button type='button'>Play/Pause</button>{' '}
+            <button type='button'>Reset</button>
+          </div>
+        </main>
+        <footer>
+          Footer Text
+        </footer>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/InputSpinner/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './InputSpinner.module.css'
+
+const InputSpinner = () => {
+    return (
+        <div className={styles.container}>InputSpinner</div>
+    )
+}
+
+export default InputSpinner
